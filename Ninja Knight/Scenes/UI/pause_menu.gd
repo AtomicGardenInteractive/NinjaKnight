e8e6b8ad5174d5ab05e6bf827261dccaa2c46b82
Brooke Heathcock <AtomicGardenInteractive@Gmail.com>
2023-11-27 22:03:41 +0000

    Pause menu in

--- Ninja Knight/Scenes/UI/pause_menu.gd
+++ Ninja Knight/Scenes/UI/pause_menu.gd
@@ -1,16 +1,15 @@
 extends Control
 
 @export var game_handler : GameHandler
-@export var pause_menu : Panel
-@export var options_menu : Panel
-@export var load_menu : Panel
+@export var options_menu : CanvasLayer
+@export var load_menu : CanvasLayer
 
+var vis_state: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
 	game_handler.connect("toggle_game_paused", _on_game_handler_toggle_game_paused)
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
@@ -18,9 +17,17 @@ func _process(_delta):
 func _on_game_handler_toggle_game_paused(is_paused: bool):
 	if(is_paused):
 		show()
+		vis_state = true
 	else:
 		hide()
+		vis_state = false
 
+func switch_menu():
+	vis_state = !vis_state
+	if vis_state:
+		show()
+	else:
+		hide()
 
 func _on_resume_pressed():
 	game_handler.game_paused = false
@@ -31,4 +38,10 @@ func _on_quit_pressed():
 
 
 func _on_load_pressed():
-	pass
+	switch_menu()
+	game_handler.switch_menu_load()
+
+
+func _on_options_pressed():
+	switch_menu()
+	game_handler.switch_menu_options()
--- Ninja Knight/Scenes/game_handler.gd
+++ Ninja Knight/Scenes/game_handler.gd
@@ -4,6 +4,11 @@ class_name GameHandler
 
 signal toggle_game_paused(is_paused : bool)
 
+@export var pause_menu: Control
+@export var load_menu: Control
+@export var options_menu: Control
+
+
 var game_paused: bool = false:
 	get:
 		return game_paused
@@ -15,3 +20,12 @@ var game_paused: bool = false:
 func _input(event: InputEvent):
 	if(event.is_action_pressed("pause")):
 		game_paused = !game_paused
+
+func switch_menu_pause():
+	pause_menu.switch_menu()
+
+func switch_menu_load():
+	load_menu.switch_menu()
+
+func switch_menu_options():
+	options_menu.switch_menu()
